48f58558184933152769ac9bb6420eda16d65f27
Satoshi Yamano <yamanosatoshi@gmail.com>
2021-04-19 23:33:19 +0900

    installer add .tmux.conf

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 #erase old setting
 rm -rf ~/.vimrc
 rm -rf ~/.vim/
+rm -rf ~/.tmux.conf
 
 #link new setting
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 #install basic plubins
 mkdir -p ~/.vim/bundle
