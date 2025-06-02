c0550771d66ecd253ceb00ef4a8051924b7519a0
Satoshi Yamano <yamanosatoshi@gmail.com>
2025-06-02 15:47:51 +0900

    Refactor scripts

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-#erase old setting
-rm -rf ~/.vimrc
-rm -rf ~/.vim/
-rm -rf ~/.tmux.conf
+set -e
 
-#link new setting
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+FILES=(.vimrc .vim .tmux.conf .gvimrc)
+
+backup_file() {
+  local target="$1"
+  local path="$HOME/$target"
+  if [ -e "$path" ] || [ -L "$path" ]; then
+    local backup="${path}.backup.$(date +%s)"
+    echo "Backing up $path to $backup"
+    mv "$path" "$backup"
+  fi
+}
+
+create_link() {
+  local target="$1"
+  ln -sfn "$DOTFILES_DIR/$target" "$HOME/$target"
+  echo "Linked $target"
+}
+
+for f in "${FILES[@]}"; do
+  backup_file "$f"
+  create_link "$f"
+done
+
+echo "Dotfiles installation complete."
